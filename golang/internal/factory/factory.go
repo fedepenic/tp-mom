40bb7e193dda3f37d9a77a74f6a8fd3233420640
fedepenic <fpenic@fi.uba.ar>
2026-03-28 17:45:35 -0300

    feat: implement queue middleware using RabbitMQ named queues

--- golang/internal/factory/factory.go
+++ golang/internal/factory/factory.go
@@ -2,11 +2,12 @@ package factory
 
 import (
 	ex "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/factory/exchange"
+	q "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/factory/queue"
 	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
 )
 
 func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
-	return nil, nil
+	return q.NewQueueMiddleware(queueName, connectionSettings)
 }
 
 func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings m.ConnSettings) (m.Middleware, error) {
---
+++ golang/internal/factory/queue/middleware.go
@@ -0,0 +1,119 @@
+package queue
+
+import (
+	"context"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
+)
+
+var _ m.Middleware = (*QueueMiddleware)(nil)
+
+type QueueMiddleware struct {
+	conn      *amqp.Connection
+	ch        *amqp.Channel
+	queueName string
+	stopCh    chan struct{}
+}
+
+func NewQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
+	connStr := fmt.Sprintf("amqp://guest:guest@%s:%d/", connectionSettings.Hostname, connectionSettings.Port)
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", m.ErrMessageMiddlewareDisconnected, err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("%w: %v", m.ErrMessageMiddlewareDisconnected, err)
+	}
+
+	_, err = ch.QueueDeclare(
+		queueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		ch.Close()
+		conn.Close()
+		return nil, fmt.Errorf("%w: %v", m.ErrMessageMiddlewareMessage, err)
+	}
+
+	return &QueueMiddleware{
+		conn:      conn,
+		ch:        ch,
+		queueName: queueName,
+		stopCh:    make(chan struct{}),
+	}, nil
+}
+
+func (qm *QueueMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
+	deliveries, err := qm.ch.Consume(
+		qm.queueName,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("%w: %v", m.ErrMessageMiddlewareDisconnected, err)
+	}
+
+	for {
+		select {
+		case <-qm.stopCh:
+			return nil
+		case delivery, ok := <-deliveries:
+			if !ok {
+				return m.ErrMessageMiddlewareDisconnected
+			}
+			msg := m.Message{Body: string(delivery.Body)}
+			ack := func() { delivery.Ack(false) }
+			nack := func() { delivery.Nack(false, true) }
+			callbackFunc(msg, ack, nack)
+		}
+	}
+}
+
+func (qm *QueueMiddleware) StopConsuming() {
+	select {
+	case <-qm.stopCh:
+	default:
+		close(qm.stopCh)
+	}
+}
+
+func (qm *QueueMiddleware) Send(msg m.Message) error {
+	if err := qm.ch.PublishWithContext(
+		context.Background(),
+		"",
+		qm.queueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "text/plain",
+			DeliveryMode: amqp.Persistent,
+			Body:         []byte(msg.Body),
+		},
+	); err != nil {
+		return fmt.Errorf("%w: %v", m.ErrMessageMiddlewareMessage, err)
+	}
+	return nil
+}
+
+func (qm *QueueMiddleware) Close() error {
+	if err := qm.ch.Close(); err != nil {
+		return m.ErrMessageMiddlewareClose
+	}
+	if err := qm.conn.Close(); err != nil {
+		return m.ErrMessageMiddlewareClose
+	}
+	return nil
+}
